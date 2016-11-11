c790b9a5bc09ff5d9415e1e3a80735e588c40580
Chintan Kotadia <chintankotadia13@gmail.com>
2016-11-11 18:08:15 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute  } from 'react-router';
+import App from './App';
+import './assets/css/styles.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
