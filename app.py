6bcc12fd7176ac0857d8dc7026e181622f08698b
Aristedes Maniatis <ari@ish.com.au>
2016-11-15 22:09:26 +1100

    Start getting python to return json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/data/people/view/<int:id>')
+def view_person(id):
+    return jsonify(
+	      id=id,
+	      name="Chintan Kotadia",
+	      username="chintankotadia13@gmail.com",
+	      company="ish",
+	      company_role="html/css coder",
+	      phone="49874646",
+	      notes="My notes",
+	      mobile="9497654654"
+	)
 
 
 if __name__ == '__main__':
