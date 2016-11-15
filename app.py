743d54963c9e4a34721299bdb398b646ac42f83b
Aristedes Maniatis <ari@ish.com.au>
2016-11-15 22:29:18 +1100

    Clean up test files and replace everything with python generated responses

--- app.py
+++ app.py
@@ -10,14 +10,42 @@ def index():
 @app.route('/data/people/view/<int:id>')
 def view_person(id):
     return jsonify(
-	      id=id,
-	      name="Chintan Kotadia",
-	      username="chintankotadia13@gmail.com",
-	      company="ish",
-	      company_role="html/css coder",
-	      phone="49874646",
-	      notes="My notes",
-	      mobile="9497654654"
+	{
+	  "status": "success",
+	  "output": {
+	    "people": {
+	      "id": id,
+	      "name": "Chintan Kotadia",
+	      "username":"chintankotadia13@gmail.com",
+	      "company": "ish",
+	      "company_role":"html/css coder",
+	      "phone":"49874646",
+	      "notes":"My notes",
+	      "mobile":"9497654654"
+	    }
+	  }
+	}
+	)
+
+@app.route('/data/people/update/<int:id>')
+def update_person(id):
+    return jsonify(
+	{
+	  "status": "success",
+	  "output": {
+	    "message": "People updated successfully",
+	    "people": {
+	      "id": "1",
+	      "name": "Chintan Kotadia",
+	      "username":"chintankotadia13@gmail.com",
+	      "company": "ish",
+	      "company_role":"html/css coder",
+	      "phone":"49874646",
+	      "notes":"My notes",
+	      "mobile":"9497654654"
+	    }
+	  }
+	}
 	)
 
 @app.route('/data/people/delete/<int:id>')
@@ -26,6 +54,27 @@ def delete_person(id):
 	  	'output':{
 	    'message':"People deleted successfully"
   	}})
+
+@app.route('/data/company/get/<search>')
+def search_company(search):
+    return jsonify(
+	{
+	  "status": "success",
+	  "output": {
+	    "message": "Companies fetched successfully",
+	    "output": [
+	      {
+	        "id": "1",
+	        "name": "ish"
+	      },
+	      {
+	        "id": "2",
+	        "name": "weasydney"
+	      }
+	    ]
+	  }
+	}
+  	)
 
 if __name__ == '__main__':
     app.run(debug=True)
