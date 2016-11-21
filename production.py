44709356311ddd63cee66090327f10293f73d81b
Aristedes Maniatis <ari@ish.com.au>
2016-11-22 00:19:06 +1100

    Not sure what crossdomain is here for, but it stops the server running

--- production.py
+++ production.py
@@ -11,7 +11,6 @@ def index():
   return render_template('index.html')
 
 @app.route('/peoples', methods=['GET', 'OPTIONS'])
-@crossdomain(origin='http://localhost:3000')
 def get_persons():
   return jsonify({
     "status" : "success",
