2f99d4bd6e37c59a5d6c6a9a6ae7ac6251a2d9d8
Aristedes Maniatis <ari@ish.com.au>
2016-11-27 10:12:15 +1100

    Cleanup of whitespace and layout to suit python standards.

--- app.py
+++ app.py
@@ -1,79 +1,81 @@
 #!/usr/bin/env /usr/local/bin/python2
 
-from flask import Flask, render_template, jsonify, make_response, request, current_app
-from datetime import timedelta
-from functools import update_wrapper
+from flask import Flask, jsonify, request
 from ldap_api import *
 
 app = Flask(__name__, static_folder='build', static_url_path='')
 
 ish_ldap = connect_to_ldap()
 
+
 @app.route('/')
 def root():
   return app.send_static_file('index.html')
 
+
 @app.route('/peoples', methods=['GET', 'OPTIONS'])
 def get_persons():
   return jsonify({
-    "status" : "success",
-    "output" : [
+    "status": "success",
+    "output": [
       {
         "id": "1",
-        "name" : "Chintan Kotadia",
-        "username" :"chintankotadia13@gmail.com",
-        "company" : "ish",
-        "company_role" :"html/css coder",
-        "phone" :"49874646",
-        "notes" :"My notes",
-        "mobile" :"9497654654"
+        "name": "Chintan Kotadia",
+        "username": "chintankotadia13@gmail.com",
+        "company": "ish",
+        "company_role": "html/css coder",
+        "phone": "49874646",
+        "notes": "My notes",
+        "mobile": "9497654654"
       },
       {
         "id": "2",
-        "name" : "Marcus Hodgson",
-        "username" :"marcus@ish.com",
-        "company" : "ish",
-        "company_role" :"developer",
-        "phone" :"987897544",
-        "notes" :"Not available",
-        "mobile" :"9797464876"
+        "name": "Marcus Hodgson",
+        "username": "marcus@ish.com",
+        "company": "ish",
+        "company_role": "developer",
+        "phone": "987897544",
+        "notes": "Not available",
+        "mobile": "9797464876"
       },
       {
         "id": "3",
-        "name" : "Stephen McIlwaine",
-        "username" :"Stephen@ish.com",
-        "company" : "ish",
-        "company_role" :"java developer",
-        "phone" :"5464979646",
-        "notes" :"Busy",
-        "mobile" :"9797464797"
+        "name": "Stephen McIlwaine",
+        "username": "Stephen@ish.com",
+        "company": "ish",
+        "company_role": "java developer",
+        "phone": "5464979646",
+        "notes": "Busy",
+        "mobile": "9797464797"
       },
       {
         "id": "4",
-        "name" : "Aristedes Maniatis",
-        "username" :"ari@ish.com.au",
-        "company" : "ish",
-        "company_role" :"developer",
-        "phone" :"554879645",
-        "notes" :"employees scrum",
-        "mobile" :"9849476469"
+        "name": "Aristedes Maniatis",
+        "username": "ari@ish.com.au",
+        "company": "ish",
+        "company_role": "developer",
+        "phone": "554879645",
+        "notes": "employees scrum",
+        "mobile": "9849476469"
       }
     ]
   })
 
-@app.route('/data/people/view/<int:id>')
-def view_person(id):
+
+@app.route('/data/people/view/<int:person_id>')
+def view_person(person_id):
   return jsonify({
     "status": "success",
     "output": {
-      "people": get_person_from_ldap(ish_ldap, id)
+      "people": get_person_from_ldap(ish_ldap, person_id)
     }
   })
 
-@app.route('/data/people/update/<int:id>', methods=['PATCH'])
-def update_person(id):
-  result = modify_person(ish_ldap, id, request.json)
-  if result == None:
+
+@app.route('/data/people/update/<int:person_id>', methods=['PATCH'])
+def update_person(person_id):
+  result = modify_person(ish_ldap, person_id, request.json)
+  if result is None:
     return jsonify({
       "status": "error",
       "message": "Person not found"
@@ -86,10 +88,11 @@ def update_person(id):
       }
   })
 
-@app.route('/data/companies/update/<int:id>', methods=['PATCH'])
-def update_company(id):
-  result = modify_company(ish_ldap, id, request.json)
-  if result == None:
+
+@app.route('/data/companies/update/<int:company_id>', methods=['PATCH'])
+def update_company(company_id):
+  result = modify_company(ish_ldap, company_id, request.json)
+  if result is None:
     return jsonify({
       "status": "error",
       "message": "Company not found"
@@ -102,12 +105,16 @@ def update_company(id):
       }
   })
 
-@app.route('/data/people/delete/<int:id>')
-def delete_person(id):
-  return jsonify(  { 'status':"success",
-    'output':{
-    'message':"People deleted successfully"
-  }})
+
+@app.route('/data/people/delete/<int:person_id>')
+def delete_person(person_id):
+  return jsonify({
+    'status': "success",
+    'output': {
+      'message': "Person %s deleted successfully" % person_id
+    }
+  })
+
 
 @app.route('/data/search/get/<search>')
 def search_entry(search):
@@ -119,11 +126,12 @@ def search_entry(search):
      }
   })
 
-@app.route('/data/company/<int:id>/people')
-def company_people(id):
-  result = get_company_people_from_ldap(ish_ldap, id)
 
-  if result == None:
+@app.route('/data/company/<int:company_id>/people')
+def company_people(company_id):
+  result = get_company_people_from_ldap(ish_ldap, company_id)
+
+  if result is None:
     return jsonify({
       "status": "error",
       "message": "Company not found"
@@ -134,14 +142,16 @@ def company_people(id):
       "peoples": result
     })
 
-@app.route('/data/company/view/<int:id>')
-def view_company(id):
+
+@app.route('/data/company/view/<int:company_id>')
+def view_company(company_id):
   return jsonify({
     "status": "success",
     "output": {
-      "company": get_company_from_ldap(ish_ldap, id)
+      "company": get_company_from_ldap(ish_ldap, company_id)
     }
   })
+
 
 if __name__ == '__main__':
   app.run(debug=True)
