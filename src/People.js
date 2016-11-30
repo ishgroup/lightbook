e660651b7e19131003871ac218eb9a4d0d8aeca5
Chintan Kotadia <chintankotadia13@gmail.com>
2016-11-30 19:12:58 +0530

    [issue #3] Updated user list view for user's more details

--- src/People.js
+++ src/People.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Icon from 'react-fa';
 import squish_logo from './assets/img/logo.png';
+import PeopleView from './PeopleView';
 
 class People extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      'peopleViewToggle': false,
+      fetch: true,
+      people: []
+    }
   }
 
   handleRemovePeople(e) {
@@ -16,29 +22,59 @@ class People extends Component {
 
   handleEditPeople(e) {
     e.preventDefault();
-    this.props.onPeopleEdit(this.props.people);
+    //this.props.onPeopleEdit(this.props.people);
     return false;
+  }
+
+  handlePeopleViewToggle() {
+    console.log('test');
+    var _toggleState = this.state.peopleViewToggle;
+    this.setState({
+      'peopleViewToggle': _toggleState === true ? false : true
+    });
+  }
+
+  handleCheckFetched(check, people=[]) {
+    console.log('handleCheckFetched');
+    console.log(check);
+    if(check !== undefined) {
+      this.setState({
+        fetch: false,
+        people: people
+      });
+    } else
+      return this.state.fetch;
   }
 
   render() {
     const _name = this.props.people.name.split(' ');
 
+    console.log(this.state.peopleViewToggle);
+
+    const _people = this.state.people.length !== 0 ? this.state.people : this.props.people;
+
     return (
       <div className="row">
-        <div className="col-xs-18">
-          <a href="" onClick={this.handleEditPeople.bind(this)} className="link-people">
-            {this.props.people.name}
-          </a>
-        </div>
+        <div className="col-xs-24" onClick={this.handlePeopleViewToggle.bind(this)}>
+          <div className="col-xs-18">
+            <a href="" onClick={this.handleEditPeople.bind(this)} className="link-people">
+              {this.props.people.name}
+            </a>
+          </div>
 
-        <div className="col-xs-6">
-          <a href={"https://squish.ish.com.au/issues/?jql=reporter%20%3D%20"+ _name[0] +"%20and%20resolution%20%3D%20Unresolved"} style={{marginRight: '5px'}} target="_blank">
-            <img src={squish_logo} width="38px" height="25px" alt="Squish Logo" />
-          </a>
-          <a href="" onClick={this.handleRemovePeople.bind(this)} style={{marginRight: '5px'}}>
-            <Icon name="remove" />
-          </a>
+          <div className="col-xs-6">
+            <a href={"https://squish.ish.com.au/issues/?jql=reporter%20%3D%20"+ _name[0] +"%20and%20resolution%20%3D%20Unresolved"} style={{marginRight: '5px'}} target="_blank">
+              <img src={squish_logo} width="38px" height="25px" alt="Squish Logo" />
+            </a>
+            <a href="" onClick={this.handleRemovePeople.bind(this)} style={{marginRight: '5px'}}>
+              <Icon name="remove" />
+            </a>
+          </div>
         </div>
+        {this.state.peopleViewToggle ?
+          <div className="view-people col-xs-24">
+            <PeopleView people={_people} checkFetched={this.handleCheckFetched.bind(this)} fetch={this.state.peopleViewToggle} />
+          </div> : ''}
       </div>
     );
   }
---
+++ src/PeopleView.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import ViewPeopleModel from './model/ViewPeopleModel';
+
+class PeopleView extends Component {
+  constructor(props) {
+    super(props);
+
+    if(this.props.checkFetched() !== false && this.props.fetch === true) {
+      ViewPeopleModel.getPeople(this, this.props.people.id, function(that, response) {
+        that.props.checkFetched(true, response.data.output.people);
+      });
+    }
+  }
+
+  isNotNull(text) {
+    return text === 'null' ? '-' : text;
+  }
+
+  render() {
+    const inputStyle = {padding: '12px'};
+
+    return (
+      <div className="row">
+        <div className="col-lg-24">
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Name:</div>
+            <div className="col-sm-18">{this.isNotNull(this.props.people.name)}</div>
+          </div>
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Email:</div>
+            <div className="col-sm-18">{this.isNotNull(this.props.people.username)}</div>
+          </div>
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Company:</div>
+            <div className="col-sm-18">{this.isNotNull(this.props.people.company)}</div>
+          </div>
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Company Role:</div>
+            <div className="col-sm-18">{this.isNotNull(this.props.people.company_role)}</div>
+          </div>
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Phone:</div>
+            <div className="col-sm-18">{this.isNotNull((this.props.people.phone !== undefined && this.props.people.phone !== null) ? this.props.people.phone.join(', ') : '')}</div>
+          </div>
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Notes:</div>
+            <div className="col-sm-18">{this.isNotNull(this.props.people.notes)}</div>
+          </div>
+          <div className="col-xs-24" style={inputStyle}>
+            <div className="col-sm-6">Mobile:</div>
+            <div className="col-sm-18">{this.isNotNull(this.props.people.mobile)}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PeopleView;
